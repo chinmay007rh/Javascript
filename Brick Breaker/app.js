caa558b2154ad27e74101a6a43b19fdd2520b357
chinmay007rh <chinmay007rh@gmail.com>
2023-04-22 13:31:43 -0500

    added user

--- Brick Breaker/app.js
+++ Brick Breaker/app.js
@@ -1,9 +1,18 @@
 const grid = document.querySelector('.grid')
 const brickWidth = 100
 const brickHeight = 20
+const ballDiameter=20
+const boardwidth =560
+const boardHeight =300
+let timerId
+let xDirection =2
+let yDirection =2
 
 const userStart = [230, 10]
 let currentPosition = userStart
+
+const ballStart =[260,30]
+let currentBallPosition = ballStart
 
 class Brick {
   constructor(xAxis, yAxis) {
@@ -48,9 +57,86 @@ function addBrick() {
 
 addBrick()
 
-const userStick = document.createElement('div')
-userStick.classList.add('userStick')
-userStick.style.left = currentPosition[0] + 'px'
-userStick.style.bottom = currentPosition[1] + 'px'
-grid.appendChild(userStick)
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
 
+
+function drawUser(){
+  user.style.left = currentPosition[0] + 'px'
+  user.style.bottom = currentPosition[1] + 'px'
+}
+
+function drawBall(){
+  ball.style.left = currentBallPosition[0] + 'px'
+  ball.style.bottom = currentBallPosition[1] + 'px'
+}
+
+function moveUser(e){
+  switch(e.key){
+    
+    case 'ArrowLeft':
+      if(currentPosition[0]>0){
+      currentPosition[0] -= 10
+      drawUser()
+      }
+      break;
+      
+      case 'ArrowRight':
+        if(currentPosition[0]<boardwidth -brickWidth)
+        currentPosition[0] += 10
+        drawUser()
+        break;
+    }
+  
+}
+
+document.addEventListener('keydown',moveUser)
+
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+function moveBall(){
+  currentBallPosition[0] += xDirection
+  currentBallPosition[1] += yDirection
+  drawBall()
+  checkForCollision()
+}
+
+timerId= setInterval(moveBall,30)
+
+function checkForCollision(){
+  if (currentBallPosition[0] >= (boardwidth - ballDiameter) || 
+      currentBallPosition[1] >= (boardHeight - ballDiameter)||
+      currentBallPosition[0] <= 0)
+
+  {
+    changeDirection()
+  }
+  if(currentBallPosition[1] <=0){
+    clearInterval(timerId)
+  }
+}
+
+function changeDirection(){
+
+  if(xDirection ===2 && yDirection ===2){
+    yDirection =-2
+    return
+  }
+  if(xDirection ===2 && yDirection === -2){
+    xDirection =-2
+    return
+  }
+  if(xDirection === -2 && yDirection === -2){
+    yDirection =2
+    return
+  }
+  if(xDirection === -2 && yDirection ===2){
+    xDirection =2
+    return
+  }
+}

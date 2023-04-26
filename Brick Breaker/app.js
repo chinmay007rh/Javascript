90d84e1b9250b9da149cd3618fb8892a659fb572
chinmay007rh <chinmay007rh@gmail.com>
2023-04-26 10:33:04 -0500

    completed basic functionality

--- Brick Breaker/app.js
+++ Brick Breaker/app.js
@@ -1,142 +1,181 @@
 const grid = document.querySelector('.grid')
-const brickWidth = 100
-const brickHeight = 20
-const ballDiameter=20
-const boardwidth =560
-const boardHeight =300
-let timerId
-let xDirection =2
-let yDirection =2
+const scoreDisplay = document.querySelector('#score')
+const blockWidth = 100
+const blockHeight = 20
+const ballDiameter = 20
+const boardWidth = 560
+const boardHeight = 300
+let xDirection = -2
+let yDirection = 2
 
 const userStart = [230, 10]
 let currentPosition = userStart
 
-const ballStart =[260,30]
-let currentBallPosition = ballStart
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
 
-class Brick {
+let timerId
+let score = 0
+
+//my block
+class Block {
   constructor(xAxis, yAxis) {
     this.bottomLeft = [xAxis, yAxis]
-    this.bottomRight = [xAxis + brickWidth, yAxis]
-    this.topLeft = [xAxis, yAxis + brickHeight]
-    this.topRight = [xAxis + brickWidth, yAxis + brickHeight]
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    this.topLeft = [xAxis, yAxis + blockHeight]
   }
 }
 
-const bricks = [
-    new Brick(10, 270),
-    new Brick(120, 270),
-    new Brick(230, 270),
-    new Brick(340, 270),
-    new Brick(450, 270),
-    new Brick(10, 240),
-    new Brick(120, 240),
-    new Brick(230, 240),
-    new Brick(340, 240),
-    new Brick(450, 240),
-    new Brick(10, 210),
-    new Brick(120, 210),
-    new Brick(230, 210),
-    new Brick(340, 210),
-    new Brick(450, 210),
-
-    
+//all my blocks
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
 ]
 
-console.log(bricks)
-
-function addBrick() {
-  for (let i = 0; i < bricks.length; i++) {
-    const brickElement = document.createElement('div')
-    brickElement.classList.add('brick')
-    brickElement.style.left = bricks[i].bottomLeft[0] + 'px'
-    brickElement.style.bottom = bricks[i].bottomLeft[1] + 'px'
-    grid.appendChild(brickElement)
+//draw my blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'  
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'  
+    grid.appendChild(block)
+    console.log(blocks[i].bottomLeft)
   }
 }
+addBlocks()
 
-addBrick()
-
+//add user
 const user = document.createElement('div')
 user.classList.add('user')
-drawUser()
 grid.appendChild(user)
+drawUser()
 
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+grid.appendChild(ball)
+drawBall()
 
-function drawUser(){
+//move user
+function moveUser(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10
+        console.log(currentPosition[0] > 0)
+        drawUser()   
+      }
+      break
+    case 'ArrowRight':
+      if (currentPosition[0] < (boardWidth - blockWidth)) {
+        currentPosition[0] += 10
+        console.log(currentPosition[0])
+        drawUser()   
+      }
+      break
+  }
+}
+document.addEventListener('keydown', moveUser)
+
+//draw User
+function drawUser() {
   user.style.left = currentPosition[0] + 'px'
   user.style.bottom = currentPosition[1] + 'px'
 }
 
-function drawBall(){
-  ball.style.left = currentBallPosition[0] + 'px'
-  ball.style.bottom = currentBallPosition[1] + 'px'
+//draw Ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + 'px'
+  ball.style.bottom = ballCurrentPosition[1] + 'px'
 }
 
-function moveUser(e){
-  switch(e.key){
-    
-    case 'ArrowLeft':
-      if(currentPosition[0]>0){
-      currentPosition[0] -= 10
-      drawUser()
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+timerId = setInterval(moveBall, 30)
+
+//check for collisions
+function checkForCollisions() {
+  //check for block collision
+  for (let i = 0; i < blocks.length; i++){
+    if
+    (
+      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+      ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) 
+    )
+      {
+      const allBlocks = Array.from(document.querySelectorAll('.block'))
+      allBlocks[i].classList.remove('block')
+      blocks.splice(i,1)
+      changeDirection()   
+      score++
+      scoreDisplay.innerHTML = score
+      if (blocks.length == 0) {
+        scoreDisplay.innerHTML = 'You Win!'
+        clearInterval(timerId)
+        document.removeEventListener('keydown', moveUser)
       }
-      break;
-      
-      case 'ArrowRight':
-        if(currentPosition[0]<boardwidth -brickWidth)
-        currentPosition[0] += 10
-        drawUser()
-        break;
     }
-  
-}
-
-document.addEventListener('keydown',moveUser)
-
-const ball = document.createElement('div')
-ball.classList.add('ball')
-drawBall()
-grid.appendChild(ball)
-
-function moveBall(){
-  currentBallPosition[0] += xDirection
-  currentBallPosition[1] += yDirection
-  drawBall()
-  checkForCollision()
-}
-
-timerId= setInterval(moveBall,30)
-
-function checkForCollision(){
-  if (currentBallPosition[0] >= (boardwidth - ballDiameter) || 
-      currentBallPosition[1] >= (boardHeight - ballDiameter)||
-      currentBallPosition[0] <= 0)
-
+  }
+  // check for wall hits
+  if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[0] <= 0 || ballCurrentPosition[1] >= (boardHeight - ballDiameter))
   {
     changeDirection()
   }
-  if(currentBallPosition[1] <=0){
+
+  //check for user collision
+  if
+  (
+    (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+    (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight ) 
+  )
+  {
+    changeDirection()
+  }
+
+  //game over
+  if (ballCurrentPosition[1] <= 0) {
     clearInterval(timerId)
+    scoreDisplay.innerHTML = 'You lose!'
+    document.removeEventListener('keydown', moveUser)
   }
 }
 
-function changeDirection(){
 
-  if(xDirection ===2 && yDirection ===2){
-    yDirection =-2
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2
     return
   }
-  if(xDirection ===2 && yDirection === -2){
-    xDirection =-2
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2
     return
   }
-  if(xDirection === -2 && yDirection === -2){
-    yDirection =2
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2
     return
   }
-  if(xDirection === -2 && yDirection ===2){
-    xDirection =2
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2
     return
   }
 }
